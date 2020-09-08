0a54c68ed5d442c280921340043841f1bac409b3
jweir136 <31812897+jweir136@users.noreply.github.com>
2020-09-08 00:30:53 -0400

    Create Candidate.h

---
+++ Candidate.h
@@ -0,0 +1,16 @@
+#ifndef CANDIDATE_H
+#define CANDIDATE_H
+
+using namespace std;
+
+class Candidate {
+  public:
+    Candidate(string name);
+
+    string getName();
+
+  private:
+    string name;
+};
+
+#endif
