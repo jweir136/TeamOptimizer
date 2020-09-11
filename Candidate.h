a4c75c1b87f185b58269f95017231a773481cc81
jweir136 <31812897+jweir136@users.noreply.github.com>
2020-09-11 16:16:12 -0400

    Update Candidate.h

--- Candidate.h
+++ Candidate.h
@@ -3,14 +3,27 @@
 
 using namespace std;
 
+/*****************************************
+* CREATE A CLASS TO REPRESENT JOB       **
+*   CANDIDATES, THEIR NAMES, AND        **
+*   THEIR RESPONSES TO THE QUESTIONS    **
+*****************************************/
 class Candidate {
   public:
-    Candidate(string name);
+    Candidate(string name, int yearsOfWorkExperience, int degree, int hasCPPExperience);
 
     string getName();
 
+    double calculateFitnessScore();
+
   private:
     string name;
+
+    int yearsOfWorkExperience;
+
+    int degree;
+
+    int hasCPPExperience;
 };
 
 #endif
