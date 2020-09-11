594fd78b43abf6a9f28ebc6f7ec5a84ed4e55dea
jweir136 <31812897+jweir136@users.noreply.github.com>
2020-09-11 16:15:36 -0400

    Update Candidate.cpp

--- Candidate.cpp
+++ Candidate.cpp
@@ -4,10 +4,17 @@
 
 using namespace std;
 
-Candidate::Candidate(string name) {
+Candidate::    Candidate(string name, int yearsOfWorkExperience, int degree, int hasCPPExperience) {
   this->name = name;
+  this->yearsOfWorkExperience = yearsOfWorkExperience;
+  this->degree = degree;
+  this->hasCPPExperience = hasCPPExperience;
 }
 
 string Candidate::getName() {
   return this->name;
+}
+
+double Candidate::calculateFitnessScore() {
+  return (0.5 * this->yearsOfWorkExperience) + (0.45 * this->degree) + (0.05 * this->hasCPPExperience);
 }
